018f7d1aea67d4fd984f86329044add4910a451c
Lipindim <lipindim@gmail.com>
2017-11-08 09:28:39 +1000

    Добавил новый уровень

--- Labyrinth/LabyrinthObjects/Finish.Designer.cs
+++ Labyrinth/LabyrinthObjects/Finish.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::Labyrinth.Properties.Resources.Finish2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.Name = "Finish";
             this.Size = new System.Drawing.Size(92, 42);
